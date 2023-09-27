b0ecb6d98f4606d8d1a76913b909973828b40995
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-09-26 21:04:51 -0300

    Feat: aula 5

--- aula-testes-4/tests/app.test.ts
+++ aula-testes-4/tests/app.test.ts
@@ -7,8 +7,8 @@ describe("Health check", () =>{
    it("/health", async () => {
       const result = await server.get("/health");
       console.log(result);
-
       const statusCode = result.status;
       expect(statusCode).toBe(200); 
    });
+   
 });
